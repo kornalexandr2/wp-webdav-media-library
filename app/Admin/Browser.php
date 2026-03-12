d22cf03e2d7ce8d5af9d42c00f7be154a8cdcc0f
git-korn-admin <kornalexandr2@yandex.ru>
2026-03-12 11:29:14 +0300

    feat: добавлена детальная отладка для браузера файлов

--- app/Admin/Browser.php
+++ app/Admin/Browser.php
@@ -40,19 +40,44 @@ class Browser {
 					<div class="wwml-loading"><?php esc_html_e( 'Initializing...', 'wp-webdav-media-library' ); ?></div>
 				</div>
 			</div>
+
+			<div id="wwml-browser-debug-wrap" style="margin-top: 20px;">
+				<h3><?php esc_html_e( 'Browser Debug Log:', 'wp-webdav-media-library' ); ?></h3>
+				<pre id="wwml-browser-debug-log" style="background: #f0f0f1; padding: 15px; border: 1px solid #c3c4c7; overflow: auto; max-height: 300px; font-family: monospace; white-space: pre-wrap; font-size: 12px;"></pre>
+			</div>
 		</div>
 
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
-				// If we are on this page, we manually trigger the view
+				function log(msg) {
+					var $log = $('#wwml-browser-debug-log');
+					$log.append('[' + new Date().toLocaleTimeString() + '] ' + msg + "\n");
+				}
+
+				log("Starting Browser initialization...");
+
 				if ($('#wwml-browser-container').length) {
-					// We need to wait a bit for media-webdav.js to load
+					var attempts = 0;
 					var checkInterval = setInterval(function() {
+						attempts++;
 						if (window.WWML_Browser_View) {
 							clearInterval(checkInterval);
-							var view = new window.WWML_Browser_View({
-								el: $('#wwml-browser-container')
-							});
+							log("WWML_Browser_View found. Creating instance...");
+							try {
+								var view = new window.WWML_Browser_View({
+									el: $('#wwml-browser-container'),
+									debug_log: log
+								});
+								log("Browser instance created.");
+							} catch(e) {
+								log("ERROR creating Browser instance: " + e.message);
+							}
+						} else {
+							if (attempts % 10 === 0) log("Waiting for WWML_Browser_View script... (attempt " + attempts + ")");
+							if (attempts > 100) {
+								clearInterval(checkInterval);
+								log("FATAL ERROR: WWML_Browser_View script failed to load after 10 seconds.");
+							}
 						}
 					}, 100);
 				}
